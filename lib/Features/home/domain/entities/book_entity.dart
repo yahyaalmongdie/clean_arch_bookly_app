5cff5963a744f9d6069d8017f2c64b21fa1ed842
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-13 22:23:00 +0300

    book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,14 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final num prise;
+  final num rating;
+
+  BookEntity(
+      {required this.image,
+      required this.title,
+      required this.authorName,
+      required this.prise,
+      required this.rating});
+}
