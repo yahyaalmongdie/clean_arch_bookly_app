41ea8b8fa000c8fc1522336118a0edef57b5a575
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-13 22:25:25 +0300

    fix home repo

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -2,5 +2,5 @@ import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 
 abstract class HomeRepo {
   Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewesBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
 }
