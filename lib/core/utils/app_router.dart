45d050345ac09b47f9ad05a0bc3eeed9e295b3a0
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-14 23:08:12 +0300

    edit app route

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -13,7 +13,7 @@ abstract class AppRouter {
   static final router = GoRouter(
     routes: [
       GoRoute(
-        path: '/',
+        path: '/test',
         builder: (context, state) => const SplashView(),
       ),
       GoRoute(
