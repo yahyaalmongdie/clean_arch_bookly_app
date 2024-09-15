d4ba9d14abcee5179dfd9366eb3d84f5a3038bc7
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-15 16:16:22 +0300

    add either class

--- lib/Features/home/domain/repos/home_repo_impl.dart
+++ lib/Features/home/domain/repos/home_repo_impl.dart
@@ -1,17 +1,7 @@
-import 'package:bookly/Features/home/domain/entities/book_entity.dart';
-import 'package:bookly/Features/home/domain/repos/home_repo.dart';
+// import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+// import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 
-class HomeRepoImpl implements HomeRepo{
-  @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
-  }
+// class HomeRepoImpl implements HomeRepo{
 
-  @override
-  Future<List<BookEntity>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
-  }
 
-}
+// }
