cdd7682078fa01b273b539e5c6a06448de4e8407
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-17 22:49:49 +0300

    create api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio _dio;
+
+  ApiService({required Dio dio}) : _dio = dio;
+
+  final String _baseUrl = "https://www.googleapis.com/books/v1/";
+
+  Future<Map<String, dynamic>> getData({required String endPoint}) async {
+    var response = await _dio.get("$_baseUrl$endPoint");
+    return response.data;
+  }
+}
