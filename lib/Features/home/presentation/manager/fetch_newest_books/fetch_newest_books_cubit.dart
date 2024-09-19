c4df9c8fe4a2bb778dc4a849f2d87fd08115f0fa
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-19 17:37:53 +0300

    implement fetch newest  book cubit

--- lib/Features/home/presentation/manager/fetch_newest_books/fetch_newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/fetch_newest_books/fetch_newest_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
 import 'package:equatable/equatable.dart';
 
 part 'fetch_newest_books_state.dart';
 
 class FetchNewestBooksCubit extends Cubit<FetchNewestBooksState> {
-  FetchNewestBooksCubit() : super(FetchNewestBooksInitial());
+  FetchNewestBooksCubit(this.fetchNewestBooksUseCase) : super(FetchNewestBooksInitial());
+   final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+  Future<void> fetchNewestBooks() async {
+    emit(FetchNewestBooksLoading());
+    var result = await fetchNewestBooksUseCase.call();
+    result.fold((failure) {
+      emit(FetchNewestBooksFailure(errorMessage: failure.errorMessage));
+    }, (success) {
+      emit(FetchNewestBooksSuccess(data: success));
+    });
+  }
 }
