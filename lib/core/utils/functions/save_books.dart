d58319b9b02994d39262d3cd805b736a0a20c0ab
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-11-29 16:35:25 +0300

    Update save_books.dart

--- lib/core/utils/functions/save_books.dart
+++ lib/core/utils/functions/save_books.dart
@@ -1,6 +1,5 @@
   import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:hive/hive.dart';
-
 void saveBooksData({required List<BookEntity> books,required  boxName}) {
      var box = Hive.box<BookEntity>(boxName);
     box.addAll(books);
