cfb4407f4618bc074f7d623f659dc725f07b27fd
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-11-29 16:36:08 +0300

    Update simple_bloc_observer.dart

--- lib/core/utils/simple_bloc_observer.dart
+++ lib/core/utils/simple_bloc_observer.dart
@@ -1,8 +1,6 @@
 
 import 'dart:developer';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 class SimpleBlocObserver extends BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
