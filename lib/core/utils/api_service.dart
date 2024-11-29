5ac035207fc23293ade9b51bd95d825e86ce19d9
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-11-29 16:34:28 +0300

    Update api_service.dart

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,12 +1,9 @@
 import 'package:dio/dio.dart';
-
 class ApiService {
   final Dio _dio;
-
   ApiService({required Dio dio}) : _dio = dio;
 
   final String _baseUrl = "https://www.googleapis.com/books/v1/";
-
   Future<Map<String, dynamic>> getData({required String endPoint}) async {
     var response = await _dio.get("$_baseUrl$endPoint");
     return response.data;
